83e0a30c99dd3fd07f0c34813df61b54d35472aa
Dima Yanovskji <criska7@gmail.com>
2023-01-30 17:37:07 +0200

    Master
    adapted

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,15 +10,15 @@ import polygon from "../assets/img/polygon.svg";
 
 const About = () => {
   return (
-    <div className="bg-[#1B1A1F] pt-[117px] flex  text-white relative">
-      <div className="px-[80px]">
+    <div className="bg-[#1B1A1F] pt-[50px] md:pt-[117px] sm:flex  text-white relative">
+      <div className="px=[40px] md:px-[80px]">
         <div className="px-[50px]">
           <div className="font-realityHyper uppercase">
-            <h1 className="text-[105px]">Lamere</h1>
-            <p className="text-[50px] text-right pb-[20px]">Cycles</p>
+            <h1 className="xl:text-[105px] lg:text-[70px] md:text-right sm:text-[60px] text-[50px]">Lamere</h1>
+            <p className="xl:text-[50px] text-[30px] lg:text-right sm:text-center pb-[20px]">Cycles</p>
           </div>
           <div className="font-labGrotesque max-w-[388px]">
-            <p className="text-[22px]">
+            <p className="xl:text-[22px] s,:text-[18px] sm-w-[10px]">
               We build only the best, customized to your exacting demands
             </p>
             <button className="mt-[40px] py-[12px] px-[32px] bg-[#EB4C56]">
@@ -26,48 +26,48 @@ const About = () => {
             </button>
           </div>
         </div>
-        <div className="flex items-center justify-between pr-[79px]">
-          <div className="py-[85px] flex flex-col max-w-[16px] gap-[28px]">
-            <img className="cursor-pointer" src={google} alt="google" />
-            <img className="cursor-pointer" src={twitter} alt="twitter" />
-            <img className="cursor-pointer" src={linkendin} alt="linkendin" />
-            <img className="cursor-pointer" src={instagram} alt="instagram" />
+        <div className="flex items-center justify-between pr-[30px] md:pr-[79px]">
+          <div className="pl-[20px] py-[85px] flex flex-col gap-[28px]">
+            <img className="w-[16px]cursor-pointer" src={google} alt="google" />
+            <img className="w-[16px] cursor-pointer" src={twitter} alt="twitter" />
+            <img className="w-[16px] cursor-pointer" src={linkendin} alt="linkendin" />
+            <img className="w-[16px] cursor-pointer" src={instagram} alt="instagram" />
           </div>
           <img className="h-[152px] cursor-pointer" src={video} alt="video" />
         </div>
       </div>
 
-      <div className=" bg-[#2F2E30] h-[576px]">
-        <img className="absolute right-[0px] top-[40px] z-10" src={bike} alt="bike" />
-        <img className="absolute right-[0px] top-[140px]" src={bgBike} alt="bg" />
-        <img className="absolute right-[2px] bottom-[30px]" src={polygon} alt="" />
-        <img className="absolute right-[2px] bottom-[50px]" src={polygon} alt="" />
-        <img className="absolute right-[2px] bottom-[70px]" src={polygon} alt="" />
-        <img className="absolute right-[2px] bottom-[90px]" src={polygon} alt="" />
-        <img className="absolute right-[22px] bottom-[30px]" src={polygon} alt="" />
-        <img className="absolute right-[22px] bottom-[50px]" src={polygon} alt="" />
-        <img className="absolute right-[22px] bottom-[70px]" src={polygon} alt="" />
-        <img className="absolute right-[22px] bottom-[90px]" src={polygon} alt="" />
-        <img className="absolute right-[42px] bottom-[30px]" src={polygon} alt="" />
-        <img className="absolute right-[42px] bottom-[50px]" src={polygon} alt="" />
-        <img className="absolute right-[42px] bottom-[70px]" src={polygon} alt="" />
-        <img className="absolute right-[42px] bottom-[90px]" src={polygon} alt="" />
-        <img className="absolute right-[62px] bottom-[30px]" src={polygon} alt="" />
-        <img className="absolute right-[62px] bottom-[50px]" src={polygon} alt="" />
-        <img className="absolute right-[62px] bottom-[70px]" src={polygon} alt="" />
-        <img className="absolute right-[62px] bottom-[90px]" src={polygon} alt="" />
-        <img className="absolute right-[82px] bottom-[30px]" src={polygon} alt="" />
-        <img className="absolute right-[82px] bottom-[50px]" src={polygon} alt="" />
-        <img className="absolute right-[82px] bottom-[70px]" src={polygon} alt="" />
-        <img className="absolute right-[82px] bottom-[90px]" src={polygon} alt="" />
-        <img className="absolute right-[102px] bottom-[30px]" src={polygon} alt="" />
-        <img className="absolute right-[102px] bottom-[50px]" src={polygon} alt="" />
-        <img className="absolute right-[102px] bottom-[70px]" src={polygon} alt="" />
-        <img className="absolute right-[102px] bottom-[90px]" src={polygon} alt="" />
-        <img className="absolute right-[122px] bottom-[30px]" src={polygon} alt="" />
-        <img className="absolute right-[122px] bottom-[50px]" src={polygon} alt="" />
-        <img className="absolute right-[122px] bottom-[70px]" src={polygon} alt="" />
-        <img className="absolute right-[122px] bottom-[90px]" src={polygon} alt="" />
+      <div className=" bg-[#2F2E30] md:h-[576px]">
+        <img className="sm:absolute right-[0px] top-[40px] z-10 xl:w-[55%] lg:w-[50%] sm:w-[35%]" src={bike} alt="bike" />
+        <img className="sm:absolute right-[0px] top-[140px] xl:w-[45%] lg:w-[35%] md:w-[30%] w-[20%]" src={bgBike} alt="bg" />
+        <img className="sm:block hidden sm:absolute sm:right-[2px] bsm:ottom-[50px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[2px] bsm:ottom-[30px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[2px] bsm:ottom-[70px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[2px] bsm:ottom-[90px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[22px] sm:bottom-[30px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[22px] sm:bottom-[50px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[22px] sm:bottom-[70px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[22px] sm:bottom-[90px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[42px] sm:bottom-[30px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[42px] sm:bottom-[50px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[42px] sm:bottom-[70px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[42px] sm:bottom-[90px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[62px] sm:bottom-[30px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[62px] sm:bottom-[50px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[62px] sm:bottom-[70px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[62px] sm:bottom-[90px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[82px] sm:bottom-[30px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[82px] sm:bottom-[50px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[82px] sm:bottom-[70px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[82px] sm:bottom-[90px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[102px] sm:bottom-[30px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[102px] sm:bottom-[50px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[102px] sm:bottom-[70px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[102px] sm:bottom-[90px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[122px] sm:bottom-[30px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[122px] sm:bottom-[50px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[122px] sm:bottom-[70px]" src={polygon} alt="" />
+        <img className="sm:block hidden sm:absolute sm:right-[122px] sm:bottom-[90px]" src={polygon} alt="" />
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,17 +7,17 @@ import peopleIcon from "../assets/icons/people.svg";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between bg-[#1B1A1F] text-[#FFFFFF] py-[29px] px-[81px]">
-      <img className="" src={logo} alt="logo" />
-      <div className="flex gap-[52px]">
+    <div className="flex flex-col text-center sm:flex-row sm:justify-between bg-[#1B1A1F] text-[#FFFFFF] py-[29px] px-[20px] sm:px-[81px]">
+      <img className="pt-[15px] px-[20px] w-[30%] h-[30%] md:w-[30%] lg:w-[18%]" src={logo} alt="logo" />
+      <div className="flex justify-center p-[20px] gap-[20px] sm:gap-[25px] md:gap-[52px]">
         <p>About</p>
         <p>Shop</p>
         <p>Contact</p>
       </div>
-      <div className="flex gap-[29px]">
-        <img src={findIcon} alt="" />
-        <img src={loginIcon} alt="" />
-        <img src={peopleIcon} alt="" />
+      <div className="flex gap-[29px] justify-center">
+        <img className="w-[16px]" src={findIcon} alt="" />
+        <img className="w-[16px]" src={loginIcon} alt="" />
+        <img className="w-[16px]" src={peopleIcon} alt="" />
       </div>
     </div>
   );
