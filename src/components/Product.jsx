062c82d42b33f304b2d98c0055ab427ca4d55849
Dima Yanovskji <criska7@gmail.com>
2023-02-01 14:17:27 +0200

    master
    adapted

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,39 +6,39 @@ import product4 from "../assets/img/product4.png";
 const Product = () => {
   return (
     <div>
-      <div className="flex justify-between font-butler px-[79px]">
+      <div className="lg:flex justify-between font-butler px-[25px] lg:px-[79px]">
         <div>
           <h3 className="text-[30px] lg:text-[64px] lg:max-w-[530px] text-center lg:text-left pb-[20px]">
             Explore Our Product
           </h3>
         </div>
         <div>
-          <p className="max-w-[480px] text-[22px] pr-[30px]">
+          <p className="max-w-[480px] text-[22px] lg:pr-[30px]">
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequat duis enim velit mollit.{" "}
           </p>
         </div>
       </div>
-      <div className="flex flex-wrap gap-[40px] justify-end pr-[90px]">
+      <div className="flex flex-wrap gap-[20px] lg:gap-[40px] lg:justify-center xl:justify-end lg:pr-[90px]">
         <div className="relative">
-          <img className="max-w-none" src={product1} alt="" />
-          <p className="absolute top-[320px] left-[40px]">Road helmet</p>
-          <p className="absolute top-[354px] left-[40px]">$60</p>
+          <img className="max-h-none lg:max-w-none relative" src={product1} alt="" />
+          <p className="absolute bottom-[24px] xl:bottom-[264px] left-[20px]">Road helmet</p>
+          <p className="absolute bottom-[5px] xl:bottom-[235px] left-[20px]">$60</p>
         </div>
         <div className="relative">
           <img src={product3} alt="" />
-          <p className="absolute top-[521px] left-[40px]">Road helmet</p>
-          <p className="absolute top-[555px] left-[40px]">$60</p>
+          <p className="absolute bottom-[24px] left-[20px]">Road helmet</p>
+          <p className="absolute bottom-[5px] left-[20px]">$60</p>
         </div>
         <div className="relative">
           <img src={product2} alt="" />
-          <p className="absolute top-[292px] left-[40px]">Road helmet</p>
-          <p className="absolute top-[327px] left-[40px]">$60</p>
+          <p className="absolute bottom-[24px] left-[20px]">Road helmet</p>
+          <p className="absolute bottom-[5px] left-[20px]">$60</p>
         </div>
         <div className="relative">
           <img src={product4} alt="" />
-          <p className="absolute top-[317px] left-[40px]">Road helmet</p>
-          <p className="absolute top-[352px] left-[40px]">$60</p>
+          <p className="absolute bottom-[24px] left-[20px]">Road helmet</p>
+          <p className="absolute bottom-[5px] left-[20px]">$60</p>
         </div>
       </div>
       <div className="flex justify-center py-[60px]">
